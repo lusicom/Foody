15427a8eb1a9c2997438914ac4d5bbb0d96d123e
Lucy Holub <lusicomgolub@gmail.com>
2021-12-08 21:34:38 +0200

    Dagger-Hilt Setup - NetworkModule:
    created MyApplication class
    created NetworkModule
    injected RemoteDataSource class
    created and injected Repository class
    created NetworkResult class
    created and injected MainViewModelClass

--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -1,8 +1,8 @@
-package com.example.foody
+package com.example.foody.util
 
 class Constants {
     companion object{
         const val BASE_URL = "https://api.spoonacular.com"
-        const val API_KEY = ""
+        const val API_KEY = "5b07733b8a7248eda4c95ed29ccd2fda"
     }
 }
