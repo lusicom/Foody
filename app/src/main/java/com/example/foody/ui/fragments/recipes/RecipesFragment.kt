2610166b4003039bfb224d79e88109f1aa6bde3d
Lucy Holub <lusicomgolub@gmail.com>
2021-12-13 13:11:17 +0200

    created DiffUtil created RecipesRowBinding created RecipesFragment created RecipesViewModel class

--- app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -5,20 +5,79 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.foody.viewmodels.MainViewModel
 import com.example.foody.R
+import com.example.foody.adapters.RecipesAdapter
+import com.example.foody.util.Constants.Companion.API_KEY
+import com.example.foody.util.NetworkResult
+import com.example.foody.viewmodels.RecipesViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
+@AndroidEntryPoint
 class RecipesFragment : Fragment() {
+
+    private lateinit var mView:View
+    private val mAdapter by lazy{RecipesAdapter()}
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var recipesViewModel: RecipesViewModel
+
+    @Override
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+        mView = inflater.inflate(R.layout.fragment_recipes, container, false)
 
-        view.recycler_view.showShimmer()
+        setupRecyclerView()
+        requestApiData()
 
-        return view
+        return mView
+    }
+
+    private fun requestApiData(){
+        mainViewModel.getRecipes(recipesViewModel.applyQueries())
+        mainViewModel.recipesResponse.observe(viewLifecycleOwner,{response ->
+            when(response){
+                is NetworkResult.Success ->{
+                    hideShimmerEffect()
+                    response.data?.let{mAdapter.setData(it)}
+                }
+                is NetworkResult.Error -> {
+                    hideShimmerEffect()
+                    Toast.makeText(requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT).show()
+                }
+
+                is NetworkResult.Loading ->{
+                    showShimmerEffect()
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView(){
+        mView.recycler_view.adapter = mAdapter
+        mView.recycler_view.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun showShimmerEffect(){
+        mView.recycler_view.showShimmer()
+    }
+
+    private fun hideShimmerEffect(){
+        mView.recycler_view.hideShimmer()
     }
 }
